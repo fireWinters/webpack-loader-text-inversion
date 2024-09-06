a19c790e3ebefcaf14e7f6985fa4eeb5bada8692
Diana Tang <1987567948@qq.com>
2024-09-06 17:25:42 +0800

    feat: this is ok

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: Diana Tang
+ * @Date: 2024-09-06 17:15:12
+ * @LastEditors: Diana Tang
+ * @Description: some description
+ * @FilePath: /webpack-loader-text-inversion/src/index.js
+ */
 
-import reversedContent from './example.txt';
+import reversedContent from './test.txt';
 console.log(reversedContent); // 输出反转后的内容
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: Diana Tang
+ * @Date: 2024-09-06 17:14:49
+ * @LastEditors: Diana Tang
+ * @Description: some description
+ * @FilePath: /webpack-loader-text-inversion/webpack.config.js
+ */
 const path = require('path');
 
 module.exports = {
@@ -10,7 +17,7 @@ module.exports = {
         rules: [
             {
                 test: /\.txt$/, // 匹配 .txt 文件
-                use: path.resolve(__dirname, 'reverse-text-loader.js'), // 使用自定义 loader
+                use: path.resolve(__dirname, 'reverse-loader'), // 使用自定义 loader
             },
         ],
     },
